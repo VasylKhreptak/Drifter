ae003e642ce00e2b9a5cd016981c833405506797
VasylKhreptak <vasylkhreptak12@gmail.com>
2023-03-08 10:28:29 +0200

    Imported Zenject;

--- Assets/Scripts/ObjectPooler/ObjectPooler.cs
+++ Assets/Scripts/ObjectPooler/ObjectPooler.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-namespace Plugins.ObjectPooler
+namespace ObjectPooler
 {
     public class ObjectPooler : MonoBehaviour
     {
--- Assets/Scripts/ObjectPooler/Pool.cs
+++ Assets/Scripts/ObjectPooler/Pool.cs
@@ -1,6 +1,4 @@
-using UnityEngine;
-
-namespace Plugins.ObjectPooler
+namespace ObjectPooler
 {
     public enum Pool
     {
